a2f6a27d155414c075470a6ef064baf475d984ec
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-11-05 09:33:39 -0800

    server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.get('/hello', (request, response) => {
+  response.status(200).send('Hello');
+});
+
